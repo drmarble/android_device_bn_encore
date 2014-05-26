5e834f478591c815a698f21d9ae5cfc10f8969c0
Steven Luo <steven@steven676.net>
2014-05-26 02:06:42 -0700

    recovery: move device init code into init.rc from postrecoveryboot.sh
    The CM recovery doesn't invoke postrecoveryboot.sh, so if we ever want
    to use that recovery, we need to get the basic device init code out of
    postrecoveryboot.sh.  init.recovery.${ro.hardware}.rc is used by all
    known recoveries (it's part of AOSP), so let's put the device init stuff
    there instead.
    
    Change-Id: Ib6cd279c6fb1f2a4072490b1e16ebcd59fb61467

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,26 +1,8 @@
 #!/sbin/sh
 
-# Resets the boot counter and the bcb instructions
-mkdir /rom
-mount /dev/block/mmcblk0p2 /rom
-mount -o rw,remount /rom
-
-# Zero out the boot counter
-dd if=/dev/zero of=/rom/devconf/BootCnt bs=1 count=4
-
-# Set up a "device" for the BCB
-ln -s /rom/BCB /dev/block/by-name/bcb
-
-# Symlink /system/bin to /sbin for compatibility with the reboot-recovery
-# script and other shell scripts for the regular, non-recovery environment
-rmdir /system/bin
-ln -s /sbin /system/bin
-
-# Align /sdcard and /emmc with their usage in the non-recovery environment
-rmdir /emmc
-ln -s /storage/sdcard0 /emmc
-rmdir /sdcard
-ln -s /storage/sdcard1 /sdcard
+# Ensure /rom is mounted sync (init may not support the mount flag)
+mount -o remount,sync /rom
+sync
 
 mount -t vfat -o ro,umask=0022 /dev/block/by-name/boot /boot
 # If a command file for the recovery was provided in /boot, write the arguments
