ac777ab45d514f3f4f84766542af2bbc4d6742fb
Steven Luo <steven@steven676.net>
2014-05-25 20:33:49 -0700

    recovery: add support for command files in /boot
    Android recoveries accept a list of commands to perform on startup in
    /cache/recovery/command; while this is a good location for an installed
    system to communicate with a recovery, it doesn't work for creating
    special-purpose script-driven recovery images (for initial install or
    repartitioning, for example).
    
    As a solution, introduce a command file in /boot (we have to be able to
    ship files in /boot to have a recovery image anyway, so this is a
    natural place to put it), and copy the arguments from that file to the
    bootloader control block on recovery startup so that they'll be picked
    up.
    
    Change-Id: Ic06850d02ef7d6f59f6ae99f3d0eef79b82e1857

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -22,7 +22,19 @@ ln -s /storage/sdcard0 /emmc
 rmdir /sdcard
 ln -s /storage/sdcard1 /sdcard
 
-# Run /boot/postrecoveryboot.sh if one was provided
 mount -t vfat -o ro,umask=0022 /dev/block/by-name/boot /boot
+# If a command file for the recovery was provided in /boot, write the arguments
+# to the BCB so that the recovery will find them
+if [ -f /boot/recovery-commands ]; then
+	recovery_args=""
+	while read arg; do
+		[ -z "$arg" ] && continue
+		recovery_args="$recovery_args $arg"
+	done < /boot/recovery-commands
+	if [ "$recovery_args" ]; then
+		/sbin/update_bcb.sh recovery $recovery_args
+	fi
+fi
+# Run /boot/postrecoveryboot.sh if one was provided
 [ -x /boot/postrecoveryboot.sh ] && /boot/postrecoveryboot.sh
 umount /boot
